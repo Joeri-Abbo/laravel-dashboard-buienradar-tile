3d0bb58267454f249eeb99d5889dfe73e015bb4b
Joeri Abbo <joeriabbo@hotmail.com>
2022-06-22 21:12:10 +0200

    Change dataset.

--- src/BuienradarComponent.php
+++ src/BuienradarComponent.php
@@ -18,7 +18,6 @@ class BuienradarComponent extends Component
     public function render()
     {
         return view('dashboard-skeleton-tile::tile', [
-            'myData' => MyStore::make()->getData(),
             'refreshIntervalInSeconds' => config('dashboard.tiles.skeleton.refresh_interval_in_seconds') ?? 60,
 
         ]);
