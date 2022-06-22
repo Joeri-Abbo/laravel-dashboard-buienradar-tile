0c83be64d13600a6cd66ff9806b7dddc0e3e1f0c
Joeri Abbo <joeriabbo@hotmail.com>
2022-06-22 21:38:15 +0200

    Move assets.

--- src/BuienradarServiceProvider.php
+++ src/BuienradarServiceProvider.php
@@ -12,8 +12,11 @@ class BuienradarServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
+            __DIR__ . '/../resources/assets/build' => resource_path('views/vendor/' . self::PACKAGE_NAME),
+        ], 'public');
+
+        $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/vendor/' . self::PACKAGE_NAME),
-            __DIR__ . '/../resources/assets/build' => resource_path('views/vendor/' . self::PACKAGE_NAME . '/assets'),
         ], 'dashboard-buienradar-views');
 
         $this->loadViewsFrom(__DIR__ . '/../resources/views', self::PACKAGE_NAME);
